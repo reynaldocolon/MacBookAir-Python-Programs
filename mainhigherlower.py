2cccfe82cfd0b1e993d7be5db25b4627d70ca9c6
reynaldocolon <reynaldocolon@gmail.com>
2026-03-04 22:55:14 -0500

    2 def compa compb

--- mainhigherlower.py
+++ mainhigherlower.py
@@ -18,6 +18,11 @@ def get_random_index():
 # def ran2():
 #     random_2 = random.randint(0, 49)
 #     return  random_2
+def compare_follower_a():
+    print("Compare A: ", gamedata.data[idx1]["name"] + ", " + gamedata.data[idx1]["description"] + ", " + gamedata.data[idx1]["country"] + ", " + str(gamedata.data[idx1]["follower_count"]))
+
+def compare_follower_b():
+    print("Compare B: ", gamedata.data[idx2]["name"]  + ", " + gamedata.data[idx2]["description"] + ", " + gamedata.data[idx2]["country"] + ", " + str(gamedata.data[idx2]["follower_count"]))
 
 
 # print(Compare_A, Compare_B)
@@ -29,9 +34,12 @@ play = " "
 
 while wrong_answer != 3 and play != "n":
     idx1 = get_random_index()
-    print("Compare A: ", gamedata.data[idx1]["name"] + ", " + gamedata.data[idx1]["description"] + ", " + gamedata.data[idx1]["country"] + ", " + str(gamedata.data[idx1]["follower_count"]))
+    compare_follower_a()
+    # print("Compare A: ", gamedata.data[idx1]["name"] + ", " + gamedata.data[idx1]["description"] + ", " + gamedata.data[idx1]["country"] + ", " + str(gamedata.data[idx1]["follower_count"]))
+    
     idx2 = get_random_index()
-    print("Compare B: ", gamedata.data[idx2]["name"]  + ", " + gamedata.data[idx2]["description"] + ", " + gamedata.data[idx2]["country"] + ", " + str(gamedata.data[idx2]["follower_count"]))
+    compare_follower_b()
+    # print("Compare B: ", gamedata.data[idx2]["name"]  + ", " + gamedata.data[idx2]["description"] + ", " + gamedata.data[idx2]["country"] + ", " + str(gamedata.data[idx2]["follower_count"]))
     print(" ")
 
     while idx1 == idx2:
@@ -44,21 +52,24 @@ while wrong_answer != 3 and play != "n":
     # print("user imput =", user_input)
     # print("Compare_A =", follower_a)
     # print("Compare_B =", follower_b)
-
+    
 
 # guess = False
 
     if follower_a > follower_b:
-        answer = follower_b
+        answer = follower_a
         selection ='a'
+        
     else: 
         answer = follower_b
         selection ='b'
+        compare_follower_b()
 
     # print("selection =", selection)
     # print(Compare_A, Compare_B,"answer = ", answer)
     if user_input == selection:
         print("You are right!")
+        compare_follower_a()
         correct += 1
         
         if correct % 3 == 0:
